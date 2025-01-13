bbb045ae16668a6d7b3da6ac75a2ccfa86a81cb7
John Jeffries <jeffries839@gmail.com>
2025-01-13 13:16:45 +0000

    changed response method

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method == 'POST':
+        return HttpResponse("you must of POSTed to this page")
+    else:
+        return HttpResponse(request.method)
