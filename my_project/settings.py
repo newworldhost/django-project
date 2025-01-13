e5a983929cbefe1020c6e16750cb53dd9017b295
John Jeffries <jeffries839@gmail.com>
2025-01-13 15:31:25 +0000

    heroku config added

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-glfkyo$-40!)(2mygnco+=azwe@(l0kz9(s^h+o+2klzoxhqww
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [,'.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
